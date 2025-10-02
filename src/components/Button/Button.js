1fe7cabffdbfef4adda791a2200eddd6e6a8d325
Igor Borozdov <melnikow_igor@mail.ru>
2025-10-02 17:23:51 +0500

    Added material ui button

---
+++ src/components/Button/Button.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './Button.scss'
+import {Button as ButtonUI} from '@mui/material'
+import PropTypes from 'prop-types'
+
+const Button = ({ onClick = () => {}, children, className }) => {
+  return (
+    <ButtonUI
+      variant='contained'
+      className={`button button-default ${className}`}
+      onClick={onClick}
+    >
+      {children}
+    </ButtonUI>
+  )
+}
+
+Button.propTypes = {
+  onClick: PropTypes.func,
+  children: PropTypes.any,
+  className: PropTypes.string
+}
+
+export default Button
