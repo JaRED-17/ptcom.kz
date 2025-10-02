a041fb71a3a8b3aaa094bde5d1701c527184e34e
Igor Borozdov <melnikow_igor@mail.ru>
2025-10-02 17:25:11 +0500

    Update header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,42 +4,36 @@ import logo from '../../media/Logo.png'
 import Markdown from '../Markdown'
 import messages from './Header.messages'
 import setMessages from '../../helpers/setMessages'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import Button from '../Button'
 
 const Header = () => {
   const message = setMessages(messages, 'app.header.')
+  const navigate = useNavigate()
 
   return (
     <header className='header'>
-      <div className='header__logo'>
-        <Link to={'/'}>
-          <img className='header__logo-img' src={logo} alt={message('logo')} />
-        </Link>
-        <h1 className='header__logo-text'>
-          {message('logo')}
-        </h1>
-      </div>
-      <div className='header__title'>
-        <Markdown text={message('title')} />
-      </div>
-      <div className='header__contacts'>
-        <Markdown
-          childrenClassName={{target: 'link', className: 'white-link'}}
-          text={message('contact.astana')}
-        />
-        <Markdown
-          childrenClassName={{target: 'link', className: 'white-link'}}
-          text={message('contact.almaty')}
-        />
-        <Markdown
-          childrenClassName={{target: 'link', className: 'white-link'}}
-          text={message('contact.petropavl')}
-        />
-        <div className='mailto'>
+      <div className='header__content'>
+        <div className='header__logo'>
+          <Link to={'/'}>
+            <img className='header__logo-img' src={logo} alt={message('logo')} />
+          </Link>
+        </div>
+        <div className='header__navigation'>
           <Markdown
-            childrenClassName={{target: 'link', className: 'blue-link'}}
-            text={message('contact.email')}
+            childrenClassName={{target: 'link', className: 'white-link'}}
+            text={message('navigation.links')}
           />
+        </div>
+        <div className='header__button'>
+          <Button
+            className='button-primary'
+            onClick={() => {
+              navigate('/calculation')
+            }}
+          >
+            {message('button')}
+          </Button>
         </div>
       </div>
     </header>
--- src/components/Header/Header.messages.js
+++ src/components/Header/Header.messages.js
@@ -5,24 +5,12 @@ export default defineMessages({
     id: 'app.header.logo',
     defaultMessage: 'Транспортно–экспедиторские услуги'
   },
-  'app.header.title': {
-    id: 'app.header.title',
-    defaultMessage: 'Организация железнодорожных перевозок\n\nпо странам СНГ и Прибалтии'
+  'app.header.navigation.links': {
+    id: 'app.header.navigation.links',
+    defaultMessage: '[Компания](/company/about)\n\n[Услуги](/services)\n\n[Консультации](/consultation)\n\n[Контакты](/contacts)'
   },
-  'app.header.contact.astana': {
-    id: 'app.header.contact.astana',
-    defaultMessage: '[+7 7172 978 468 - Астана](tel:87172978468)'
-  },
-  'app.header.contact.almaty': {
-    id: 'app.header.contact.almaty',
-    defaultMessage: '[+7 727 312 2115 - Алматы](tel:87273122115)'
-  },
-  'app.header.contact.petropavl': {
-    id: 'app.header.contact.petropavl',
-    defaultMessage: '[+7 7152 63 00 47 - Петропавловск](tel:+77152630047)'
-  },
-  'app.header.contact.email': {
-    id: 'app.header.contact.email',
-    defaultMessage: '[info@ptcom.kz](mailto:info@ptcom.kz)'
+  'app.header.button': {
+    id: 'app.header.button',
+    defaultMessage: 'Расчет тарифов'
   }
 })
