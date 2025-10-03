ed521ed564408bdf1be6d31635ba756959855749
Igor Borozdov <melnikow_igor@mail.ru>
2025-10-03 17:45:46 +0500

    Update home page

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,12 +1,54 @@
 import React from 'react'
 import './Home.scss'
+import setMessages from '../../helpers/setMessages'
+import messages from './Home.messages'
+import Button from '../../components/Button'
+import withRoute from '../../helpers/withRoute'
+import PropTypes from 'prop-types'
 
-const Home = () => {
+const Home = ({ navigate }) => {
+  const message = setMessages(messages, 'app.page.home.')
+
   return (
     <div className={'home'}>
-      {'home content'}
+      <div className={'home__content'}>
+        <div className={'home__content--block'}>
+          <div className={'home__content--text'}>
+            <h1>{message('title')}</h1>
+            <p>{message('text')}</p>
+          </div>
+          <div className={'home__content--buttons'}>
+            <Button
+              className='button-secondary button-services'
+              onClick={() => {
+                navigate('/services')
+              }}
+            >
+              {message('button.services')}
+            </Button>
+            <Button
+              className='button-secondary button-consultation'
+              onClick={() => {
+                navigate('/consultation')
+              }}
+            >
+              {message('button.consultation')}
+            </Button>
+          </div>
+        </div>
+      </div>
+      <div className='backgrounds curve-orange-1' />
+      <div className='backgrounds curve-orange-2' />
+      <div className='backgrounds curve-turquoise' />
+      <div className='backgrounds curve-orange-bottom-1' />
+      <div className='backgrounds curve-orange-bottom-2' />
+      <div className='backgrounds trains' />
     </div>
   )
 }
 
-export default Home
+Home.propTypes = {
+  navigate: PropTypes.object
+}
+
+export default withRoute(Home)
---
+++ src/Pages/Home/Home.messages.js
@@ -0,0 +1,20 @@
+import { defineMessages } from 'react-intl'
+
+export default defineMessages({
+  'app.page.home.title': {
+    id: 'app.page.home.title',
+    defaultMessage: 'Транспортно-экспедиторские услуги'
+  },
+  'app.page.home.text': {
+    id: 'app.page.home.text',
+    defaultMessage: 'Организация железнодорожных перевозок по странам СНГ и Прибалтии'
+  },
+  'app.page.home.button.services': {
+    id: 'app.page.home.button.services',
+    defaultMessage: 'Услуги'
+  },
+  'app.page.home.button.consultation': {
+    id: 'app.page.home.button.consultation',
+    defaultMessage: 'Консультация'
+  }
+})
