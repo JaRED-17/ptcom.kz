c7cb0068ab800d23c16c49f221b68c8c35404bdb
Igor Borozdov <melnikow_igor@mail.ru>
2025-10-04 17:46:04 +0500

    Update Contacts page

---
+++ src/Pages/Contacts/ContactWithImage.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import './ContactWithImage.scss'
+import PropTypes from 'prop-types'
+
+const ContactWithImage = ({ image, title, contact }) => {
+  return (
+    <div className={'contact-with-image'}>
+      <div className={'contact-with-image__image--block'}>
+        <img className={'contact-with-image__image'} src={`../../../media/icons/${image}.png`} alt='' />
+      </div>
+      <div className={'contact-with-image__text--block'}>
+        <div className={'contact-with-image__title'}>
+          {title}
+        </div>
+        <p className={'contact-with-image__contact'}>
+          {contact}
+        </p>
+      </div>
+    </div>
+  )
+}
+
+ContactWithImage.propTypes = {
+  image: PropTypes.string,
+  title: PropTypes.string,
+  contact: PropTypes.string
+}
+
+export default ContactWithImage
--- src/Pages/Contacts/Contacts.messages.js
+++ src/Pages/Contacts/Contacts.messages.js
@@ -5,88 +5,132 @@ export default defineMessages({
     id: 'app.page.contacts.title',
     defaultMessage: 'Контакты'
   },
-  'app.page.contacts.name1': {
-    id: 'app.page.contacts.name1',
+  'app.page.contacts.hint': {
+    id: 'app.page.contacts.hint',
+    defaultMessage: 'Для того, чтобы получить интересующую вас в компании "Петропавл Экспресс Логистика" информация свяжитесь с нами по удобным для вас телефонам'
+  },
+  'app.page.contacts.astana.title': {
+    id: 'app.page.contacts.astana.title',
+    defaultMessage: 'Для Астаны и Казахстана *'
+  },
+  'app.page.contacts.astana.contact': {
+    id: 'app.page.contacts.astana.contact',
+    defaultMessage: '+7 7172 978 468'
+  },
+  'app.page.contacts.almaty.title': {
+    id: 'app.page.contacts.almaty.title',
+    defaultMessage: 'Для Алматы *'
+  },
+  'app.page.contacts.almaty.contact': {
+    id: 'app.page.contacts.almaty.contact',
+    defaultMessage: '+7 727 312 2115'
+  },
+  'app.page.contacts.petropavl.title': {
+    id: 'app.page.contacts.petropavl.title',
+    defaultMessage: 'Для Петропавловска *'
+  },
+  'app.page.contacts.petropavl.contact': {
+    id: 'app.page.contacts.petropavl.contact',
+    defaultMessage: '+7 7152 63 00 47'
+  },
+  'app.page.contacts.sipnet.phone': {
+    id: 'app.page.contacts.sipnet.phone',
+    defaultMessage: '0042019636'
+  },
+  'app.page.contacts.sipnet.warning': {
+    id: 'app.page.contacts.sipnet.warning',
+    defaultMessage: '*Все входящие звонки маршрутизируются и принимаются в офисе компании в Петропавловске, Республика Казахстан. Телефонные номера в Астане, Алматы, Москве работают для удобства наших региональных клиентов.'
+  },
+  'app.page.contacts.address.title': {
+    id: 'app.page.contacts.address.title',
+    defaultMessage: 'Юридический и почтовый адрес нашей компании:'
+  },
+  'app.page.contacts.address.text': {
+    id: 'app.page.contacts.address.text',
+    defaultMessage: 'Товарищество с ограниченной ответственностью "Петропавл Экспресс Логистика" Казахстан, Северо-Казахстанская облась, г. Петропавловск, ул. Сатпаева, 46'
+  },
+  'app.page.contacts.list.name1': {
+    id: 'app.page.contacts.list.name1',
     defaultMessage: 'Евгения Владимировна Криворучко'
   },
-  'app.page.contacts.position1': {
-    id: 'app.page.contacts.position1',
+  'app.page.contacts.list.position1': {
+    id: 'app.page.contacts.list.position1',
     defaultMessage: 'Бухгалтер по налогам'
   },
-  'app.page.contacts.contact1': {
-    id: 'app.page.contacts.contact1',
+  'app.page.contacts.list.contact1': {
+    id: 'app.page.contacts.list.contact1',
     defaultMessage: 'Внутренний номер 315 [evgedsha_petr@mail.ru](mailto:evgedsha_petr@mail.ru)'
   },
-  'app.page.contacts.name2': {
-    id: 'app.page.contacts.name2',
+  'app.page.contacts.list.name2': {
+    id: 'app.page.contacts.list.name2',
     defaultMessage: 'Анна Андреевна Елагина'
   },
-  'app.page.contacts.position2': {
-    id: 'app.page.contacts.position2',
+  'app.page.contacts.list.position2': {
+    id: 'app.page.contacts.list.position2',
     defaultMessage: 'Начальник отдела по работе с поставщиками'
   },
-  'app.page.contacts.contact2': {
-    id: 'app.page.contacts.contact2',
+  'app.page.contacts.list.contact2': {
+    id: 'app.page.contacts.list.contact2',
     defaultMessage: 'Внутренний номер 308 [yelagina@ptcom.kz](mailto:yelagina@ptcom.kz)'
   },
-  'app.page.contacts.name3': {
-    id: 'app.page.contacts.name3',
+  'app.page.contacts.list.name3': {
+    id: 'app.page.contacts.list.name3',
     defaultMessage: 'Татьяна Игоревна Ляпустина'
   },
-  'app.page.contacts.position3': {
-    id: 'app.page.contacts.position3',
+  'app.page.contacts.list.position3': {
+    id: 'app.page.contacts.list.position3',
     defaultMessage: 'Начальник отдела железнодорожных перевозок'
   },
-  'app.page.contacts.contact3': {
-    id: 'app.page.contacts.contact3',
+  'app.page.contacts.list.contact3': {
+    id: 'app.page.contacts.list.contact3',
     defaultMessage: 'Внутренний номер 311  [lyapustina@ptcom.kz](mailto:lyapustina@ptcom.kz)'
   },
-  'app.page.contacts.name4': {
-    id: 'app.page.contacts.name4',
+  'app.page.contacts.list.name4': {
+    id: 'app.page.contacts.list.name4',
     defaultMessage: 'Татьяна Сергеевна Плятова'
   },
-  'app.page.contacts.position4': {
-    id: 'app.page.contacts.position4',
+  'app.page.contacts.list.position4': {
+    id: 'app.page.contacts.list.position4',
     defaultMessage: 'Менеджер отдела по работе с клиентами'
   },
-  'app.page.contacts.contact4': {
-    id: 'app.page.contacts.contact4',
+  'app.page.contacts.list.contact4': {
+    id: 'app.page.contacts.list.contact4',
     defaultMessage: 'Внутренний номер 304 [plyatova@ptcom.kz](mailto:plyatova@ptcom.kz)'
   },
-  'app.page.contacts.name5': {
-    id: 'app.page.contacts.name5',
+  'app.page.contacts.list.name5': {
+    id: 'app.page.contacts.list.name5',
     defaultMessage: 'Диляра Багдатовна Кондрашова'
   },
-  'app.page.contacts.position5': {
-    id: 'app.page.contacts.position5',
+  'app.page.contacts.list.position5': {
+    id: 'app.page.contacts.list.position5',
     defaultMessage: 'Менеджер отдела по работе с клиентами'
   },
-  'app.page.contacts.contact5': {
-    id: 'app.page.contacts.contact5',
+  'app.page.contacts.list.contact5': {
+    id: 'app.page.contacts.list.contact5',
     defaultMessage: 'Внутренний номер 309 [dilya@ptcom.kz](mailto:dilya@ptcom.kz)'
   },
-  'app.page.contacts.name6': {
-    id: 'app.page.contacts.name6',
+  'app.page.contacts.list.name6': {
+    id: 'app.page.contacts.list.name6',
     defaultMessage: 'Анна Владимировна Курьякова'
   },
-  'app.page.contacts.position6': {
-    id: 'app.page.contacts.position6',
+  'app.page.contacts.list.position6': {
+    id: 'app.page.contacts.list.position6',
     defaultMessage: 'Офис-менеджер'
   },
-  'app.page.contacts.contact6': {
-    id: 'app.page.contacts.contact6',
+  'app.page.contacts.list.contact6': {
+    id: 'app.page.contacts.list.contact6',
     defaultMessage: '[info@ptcom.kz](mailto:info@ptcom.kz), [corp@ptcom.kz](mailto:corp@ptcom.kz)'
   },
-  'app.page.contacts.name7': {
-    id: 'app.page.contacts.name7',
+  'app.page.contacts.list.name7': {
+    id: 'app.page.contacts.list.name7',
     defaultMessage: 'Гиберт Виктор Витальевич'
   },
-  'app.page.contacts.position7': {
-    id: 'app.page.contacts.position7',
+  'app.page.contacts.list.position7': {
+    id: 'app.page.contacts.list.position7',
     defaultMessage: 'Менеджер отдела по работе с клиентами'
   },
-  'app.page.contacts.contact7': {
-    id: 'app.page.contacts.contact7',
+  'app.page.contacts.list.contact7': {
+    id: 'app.page.contacts.list.contact7',
     defaultMessage: 'Внутренний номер 310 [manager5@ptcom.kz](mailto:manager5@ptcom.kz)'
   }
 })
