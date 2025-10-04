796a169c8725b2be3c2e54f30eae39e4afa0e39b
Igor Borozdov <melnikow_igor@mail.ru>
2025-10-04 16:09:50 +0500

    Update Contacts list

--- src/Pages/Contacts/Contacts.js
+++ src/Pages/Contacts/Contacts.js
@@ -16,12 +16,12 @@ const Contacts = () => {
           </div>
           <div className={'contacts__content--block__contacts'}>
             <ContactBlock name={message('name1')} position={message('position1')} contact={message('contact1')} />
-            <ContactBlock name={message('name1')} position={message('position1')} contact={message('contact1')} />
-            <ContactBlock name={message('name1')} position={message('position1')} contact={message('contact1')} />
-            <ContactBlock name={message('name1')} position={message('position1')} contact={message('contact1')} />
-            <ContactBlock name={message('name1')} position={message('position1')} contact={message('contact1')} />
-            <ContactBlock name={message('name1')} position={message('position1')} contact={message('contact1')} />
-            <ContactBlock name={message('name1')} position={message('position1')} contact={message('contact1')} />
+            <ContactBlock name={message('name2')} position={message('position2')} contact={message('contact2')} />
+            <ContactBlock name={message('name3')} position={message('position3')} contact={message('contact3')} />
+            <ContactBlock name={message('name4')} position={message('position4')} contact={message('contact4')} />
+            <ContactBlock name={message('name5')} position={message('position5')} contact={message('contact5')} />
+            <ContactBlock name={message('name6')} position={message('position6')} contact={message('contact6')} />
+            <ContactBlock name={message('name7')} position={message('position7')} contact={message('contact7')} />
           </div>
         </div>
       </div>
--- src/Pages/Contacts/Contacts.messages.js
+++ src/Pages/Contacts/Contacts.messages.js
@@ -16,5 +16,77 @@ export default defineMessages({
   'app.page.contacts.contact1': {
     id: 'app.page.contacts.contact1',
     defaultMessage: 'Внутренний номер 315 [evgedsha_petr@mail.ru](mailto:evgedsha_petr@mail.ru)'
+  },
+  'app.page.contacts.name2': {
+    id: 'app.page.contacts.name2',
+    defaultMessage: 'Анна Андреевна Елагина'
+  },
+  'app.page.contacts.position2': {
+    id: 'app.page.contacts.position2',
+    defaultMessage: 'Начальник отдела по работе с поставщиками'
+  },
+  'app.page.contacts.contact2': {
+    id: 'app.page.contacts.contact2',
+    defaultMessage: 'Внутренний номер 308 [yelagina@ptcom.kz](mailto:yelagina@ptcom.kz)'
+  },
+  'app.page.contacts.name3': {
+    id: 'app.page.contacts.name3',
+    defaultMessage: 'Татьяна Игоревна Ляпустина'
+  },
+  'app.page.contacts.position3': {
+    id: 'app.page.contacts.position3',
+    defaultMessage: 'Начальник отдела железнодорожных перевозок'
+  },
+  'app.page.contacts.contact3': {
+    id: 'app.page.contacts.contact3',
+    defaultMessage: 'Внутренний номер 311  [lyapustina@ptcom.kz](mailto:lyapustina@ptcom.kz)'
+  },
+  'app.page.contacts.name4': {
+    id: 'app.page.contacts.name4',
+    defaultMessage: 'Татьяна Сергеевна Плятова'
+  },
+  'app.page.contacts.position4': {
+    id: 'app.page.contacts.position4',
+    defaultMessage: 'Менеджер отдела по работе с клиентами'
+  },
+  'app.page.contacts.contact4': {
+    id: 'app.page.contacts.contact4',
+    defaultMessage: 'Внутренний номер 304 [plyatova@ptcom.kz](mailto:plyatova@ptcom.kz)'
+  },
+  'app.page.contacts.name5': {
+    id: 'app.page.contacts.name5',
+    defaultMessage: 'Диляра Багдатовна Кондрашова'
+  },
+  'app.page.contacts.position5': {
+    id: 'app.page.contacts.position5',
+    defaultMessage: 'Менеджер отдела по работе с клиентами'
+  },
+  'app.page.contacts.contact5': {
+    id: 'app.page.contacts.contact5',
+    defaultMessage: 'Внутренний номер 309 [dilya@ptcom.kz](mailto:dilya@ptcom.kz)'
+  },
+  'app.page.contacts.name6': {
+    id: 'app.page.contacts.name6',
+    defaultMessage: 'Анна Владимировна Курьякова'
+  },
+  'app.page.contacts.position6': {
+    id: 'app.page.contacts.position6',
+    defaultMessage: 'Офис-менеджер'
+  },
+  'app.page.contacts.contact6': {
+    id: 'app.page.contacts.contact6',
+    defaultMessage: '[info@ptcom.kz](mailto:info@ptcom.kz), [corp@ptcom.kz](mailto:corp@ptcom.kz)'
+  },
+  'app.page.contacts.name7': {
+    id: 'app.page.contacts.name7',
+    defaultMessage: 'Гиберт Виктор Витальевич'
+  },
+  'app.page.contacts.position7': {
+    id: 'app.page.contacts.position7',
+    defaultMessage: 'Менеджер отдела по работе с клиентами'
+  },
+  'app.page.contacts.contact7': {
+    id: 'app.page.contacts.contact7',
+    defaultMessage: 'Внутренний номер 310 [manager5@ptcom.kz](mailto:manager5@ptcom.kz)'
   }
 })
