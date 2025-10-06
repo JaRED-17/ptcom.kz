5612c1e7a9940ad78b8350b762c25fae4c663078
Igor Borozdov <melnikow_igor@mail.ru>
2025-10-06 22:39:04 +0500

    Added new pages to the sitemap

--- src/components/PageTitle/PageTitle.messages.js
+++ src/components/PageTitle/PageTitle.messages.js
@@ -5,6 +5,22 @@ export default defineMessages({
     id: 'app.page.aboutUs.title',
     defaultMessage: 'О компании'
   },
+  'app.page.staffs.title': {
+    id: 'app.page.staffs.title',
+    defaultMessage: 'Сотрудники'
+  },
+  'app.page.news.title': {
+    id: 'app.page.news.title',
+    defaultMessage: 'Новости'
+  },
+  'app.page.licenses.title': {
+    id: 'app.page.licenses.title',
+    defaultMessage: 'Лицензии'
+  },
+  'app.page.vacancies.title': {
+    id: 'app.page.vacancies.title',
+    defaultMessage: 'Вакансии'
+  },
   'app.page.services.title': {
     id: 'app.page.services.title',
     defaultMessage: 'Наши услуги'
@@ -36,6 +52,22 @@ export default defineMessages({
   'app.page.consultation.title': {
     id: 'app.page.consultation.title',
     defaultMessage: 'Консультации'
+  },
+  'app.page.dangerousGoods.title': {
+    id: 'app.page.dangerousGoods.title',
+    defaultMessage: 'Классификации опасных грузов на железнодорожном транспорте'
+  },
+  'app.page.containers.title': {
+    id: 'app.page.containers.title',
+    defaultMessage: 'Основные типы контейнеров'
+  },
+  'app.page.wagons.title': {
+    id: 'app.page.wagons.title',
+    defaultMessage: 'Общие сведения об устройстве вагонов'
+  },
+  'app.page.wagonsTypes.title': {
+    id: 'app.page.wagonsTypes.title',
+    defaultMessage: 'Основные типы вагонов'
   },
   'app.page.calculation.title': {
     id: 'app.page.calculation.title',
