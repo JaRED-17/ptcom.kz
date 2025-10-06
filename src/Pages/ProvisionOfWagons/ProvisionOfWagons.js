9ec4f4bf9bcfb5677dc138f6a3bc9751cc0d83b0
Igor Borozdov <melnikow_igor@mail.ru>
2025-10-06 17:13:55 +0500

    Update provisionOfWagons page, update list

--- src/Pages/ProvisionOfWagons/ProvisionOfWagons.js
+++ src/Pages/ProvisionOfWagons/ProvisionOfWagons.js
@@ -16,7 +16,14 @@ const ProvisionOfWagons = () => {
             <h1>{message('title')}</h1>
           </div>
           <p className={'text-color-gray'}>{message('paragraph')}</p>
-          <Markdown childrenClassName={{target: 'list', className: 'text-color-gray'}} text={message('list')} />
+          <div className={`${classNamePrefix}__content--list`}>
+            <div className={`${classNamePrefix}__list1`}>
+              <Markdown childrenClassName={{target: 'list', className: 'text-color-gray'}} text={message('list1')} />
+            </div>
+            <div className={`${classNamePrefix}__list2`}>
+              <Markdown childrenClassName={{target: 'list', className: 'text-color-gray'}} text={message('list2')} />
+            </div>
+          </div>
         </div>
       </div>
       <div className={'backgrounds provision-of-wagons-background'} />
--- src/Pages/ProvisionOfWagons/ProvisionOfWagons.messages.js
+++ src/Pages/ProvisionOfWagons/ProvisionOfWagons.messages.js
@@ -9,16 +9,19 @@ export default defineMessages({
     id: 'app.page.provisionOfWagons.paragraph',
     defaultMessage: 'Наша компания предоставляет вагоны различного назначения для организации перевозки грузов по железной дороге: крытые вагоны, полувагоны, зерновозы. Мы можем поставить вагоны на любую железнодорожную станцию в таких странах, как:'
   },
-  'app.page.provisionOfWagons.list': {
-    id: 'app.page.provisionOfWagons.list',
+  'app.page.provisionOfWagons.list1': {
+    id: 'app.page.provisionOfWagons.list1',
     defaultMessage: '- Казахстан;\n' +
       '- Российская Федерация;\n' +
       '- Таджикистан;\n' +
       '- Узбекистан;\n' +
       '- Киргизия;\n' +
       '- Армения;\n' +
-      '- Грузия;\n' +
-      '- Азербайджан;\n' +
+      '- Грузия;'
+  },
+  'app.page.provisionOfWagons.list2': {
+    id: 'app.page.provisionOfWagons.list2',
+    defaultMessage: '- Азербайджан;\n' +
       '- Белорусь;\n' +
       '- Украина;\n' +
       '- Литва;\n' +
