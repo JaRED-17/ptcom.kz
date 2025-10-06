3bf84f65683a2d08cc1c67a86bc628d1f118a041
Igor Borozdov <melnikow_igor@mail.ru>
2025-10-06 23:10:14 +0500

    Update Content component to use json

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import './Content.scss'
+import data from '../../data/pages.json'
 
 /* Pages */
 import Home from '../../Pages/Home'
@@ -20,21 +21,38 @@ import Sitemap from '../../Pages/Sitemap'
 import StyleGuide from '../../Pages/StyleGuide'
 
 const Content = () => {
+  const components = {
+    'aboutUs': <AboutUs />,
+    'services': <Services />,
+    'provisionOfWagons': <ProvisionOfWagons />,
+    'paymentRailwayTariffs': <PaymentRailwayTariffs />,
+    'driveWays': <DriveWays />,
+    'schema': <Schema />,
+    'handlingDostyk': <HandlingDostyk />,
+    'cargoProcessing': <CargoProcessing />,
+    'consultation': <Consultation />,
+    'calculation': <Calculation />,
+    'contacts': <Contacts />,
+    'sitemap': <Sitemap />
+  }
+  const { pages } = data
+
   return (
     <div className={'content'}>
       <Routes>
         <Route path={'/'} element={<Home />} exact />
-        <Route path={'/about-us'} element={<AboutUs />} exact />
-        <Route path={'/services'} element={<Services />} exact />
-        <Route path={'/services/provision-of-wagons'} element={<ProvisionOfWagons />} exact />
-        <Route path={'/services/payment-railway-tariffs'} element={<PaymentRailwayTariffs />} exact />
-        <Route path={'/services/drive-ways'} element={<DriveWays />} exact />
-        <Route path={'/services/schema'} element={<Schema />} exact />
-        <Route path={'/services/handling-dostyk'} element={<HandlingDostyk />} exact />
-        <Route path={'/services/cargo-processing'} element={<CargoProcessing />} exact />
-        <Route path={'/consultation'} element={<Consultation />} exact />
-        <Route path={'/calculation'} element={<Calculation />} exact />
-        <Route path={'/contacts'} element={<Contacts />} exact />
+        {(() => pages.map((page, index) => {
+          if (page.entries) {
+            return (
+              <>
+                <Route key={index} path={page.url} element={components[page.name] || <PageNotFound />} exact />
+                {(() => page.entries.map((page, index) => <Route key={index} path={page.url} element={components[page.name] || <PageNotFound />} exact />))()}
+              </>
+            )
+          }
+
+          return <Route key={index} path={page.url} element={components[page.name] || <PageNotFound />} exact />
+        }))()}
         <Route path={'/sitemap'} element={<Sitemap />} exact />
         <Route path={'/style-guide'} element={<StyleGuide />} exact />
         <Route path={'*'} element={<PageNotFound />} />
