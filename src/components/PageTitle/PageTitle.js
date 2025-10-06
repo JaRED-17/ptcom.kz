d4b03688e10f1569469cf0a6fee2a69fc8c13193
Igor Borozdov <melnikow_igor@mail.ru>
2025-10-06 21:53:23 +0500

    Added PageTitle component

---
+++ src/components/PageTitle/PageTitle.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Sitemap.scss'
+import setMessages from '../../helpers/setMessages'
+import messages from './PageTitle.messages'
+import PropTypes from 'prop-types'
+
+const PageTitle = ({ name }) => {
+  const classNamePrefix = 'page-title'
+  const message = setMessages(messages, 'app.page.')
+
+  return (
+    <div className={classNamePrefix}>
+      <h1 className={`${classNamePrefix}__title`}>{message(name + 'title')}</h1>
+    </div>
+  )
+}
+
+PageTitle.propTypes = {
+  name: PropTypes.string
+}
+
+export default PageTitle
