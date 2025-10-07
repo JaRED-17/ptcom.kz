6df13389c0c391592e8b496ba0611c291c91c7ed
Igor Borozdov <melnikow_igor@mail.ru>
2025-10-07 18:37:00 +0500

    Added Aside menu

---
+++ src/components/AsideMenu/AsideMenu.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import './AsideMenu.scss'
+import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
+import setMessages from '../../helpers/setMessages'
+import messages from '../PageTitle/PageTitle.messages'
+
+const AsideMenu = ({ list }) => {
+  const classNamePrefix = 'aside-menu'
+  const message = setMessages(messages, 'app.page.')
+
+  return (
+    <div className={classNamePrefix}>
+      {(() => {
+        return list.map((menuItem, index) => {
+          return <Link key={index} to={menuItem.url} className={'white-link'}>
+            {message(menuItem.name + '.title')}
+          </Link>
+        })
+      })()}
+    </div>
+  )
+}
+
+AsideMenu.propTypes = {
+  list: PropTypes.array.isRequired
+}
+
+export default AsideMenu
