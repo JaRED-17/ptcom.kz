d10141f32a2774869a110d76483c98dc69e474c6
Igor Borozdov <melnikow_igor@mail.ru>
2025-10-08 22:13:07 +0500

    Added text for the News page

--- src/Pages/News/News.js
+++ src/Pages/News/News.js
@@ -1,14 +1,42 @@
 import React from 'react'
+import './News.scss'
+import setMessages from '../../helpers/setMessages'
+import messages from './News.messages.js'
 import OurAdvantages from '../../components/OurAdvantages'
 import PageTitle from '../../components/PageTitle'
 import PageWithAsideMenu from '../../components/PageWithAsideMenu'
+import Markdown from '../../components/Markdown'
 
 const News = () => {
   const classNamePrefix = 'news'
+  const message = setMessages(messages, 'app.page.news.')
 
   return (
     <PageWithAsideMenu classNamePrefix={classNamePrefix} index={0}>
       <PageTitle name={'news'} />
+      <Markdown childrenClassName={{target: 'link', className: 'orange-link'}} text={message('header1')} />
+      <p className={'text-color-gray'}>{message('description1')}</p>
+
+      <Markdown childrenClassName={{target: 'link', className: 'orange-link'}} text={message('header2')} />
+      <p className={'text-color-gray'}>{message('description2')}</p>
+
+      <Markdown childrenClassName={{target: 'link', className: 'orange-link'}} text={message('header3')} />
+      <p className={'text-color-gray'}>{message('description3')}</p>
+
+      <Markdown childrenClassName={{target: 'link', className: 'orange-link'}} text={message('header4')} />
+      <p className={'text-color-gray'}>{message('description4')}</p>
+
+      <Markdown childrenClassName={{target: 'link', className: 'orange-link'}} text={message('header5')} />
+      <p className={'text-color-gray'}>{message('description5')}</p>
+
+      <Markdown childrenClassName={{target: 'link', className: 'orange-link'}} text={message('header6')} />
+      <p className={'text-color-gray'}>{message('description6')}</p>
+
+      <Markdown childrenClassName={{target: 'link', className: 'orange-link'}} text={message('header7')} />
+      <p className={'text-color-gray'}>{message('description7')}</p>
+
+      <Markdown childrenClassName={{target: 'link', className: 'orange-link'}} text={message('header8')} />
+      <p className={'text-color-gray'}>{message('description8')}</p>
       <OurAdvantages />
     </PageWithAsideMenu>
   )
---
+++ src/Pages/News/News.messages.js
@@ -0,0 +1,68 @@
+import { defineMessages } from 'react-intl'
+
+export default defineMessages({
+  'app.page.news.header1': {
+    id: 'app.page.news.header1',
+    defaultMessage: '[СТ РК ИСО 9001–2009 18.11.2014](/company/news/st-rk-iso-90012009)'
+  },
+  'app.page.news.description1': {
+    id: 'app.page.news.description1',
+    defaultMessage: 'Получен сертификат соответствия СТ РК ИСО 9001–2009'
+  },
+  'app.page.news.header2': {
+    id: 'app.page.news.header2',
+    defaultMessage: '[Наша компания - 5 лет на рынке! 15.04.2015](/company/news/5years)'
+  },
+  'app.page.news.description2': {
+    id: 'app.page.news.description2',
+    defaultMessage: 'Наша компания — 5 лет на рынке!'
+  },
+  'app.page.news.header3': {
+    id: 'app.page.news.header3',
+    defaultMessage: '[Участие в выставке «Транспорт и Логистика», г. Алматы. 19.09.2015](/company/news/tl2015)'
+  },
+  'app.page.news.description3': {
+    id: 'app.page.news.description3',
+    defaultMessage: 'Участие в выставке «Транспорт и Логистика», г. Алматы.'
+  },
+  'app.page.news.header4': {
+    id: 'app.page.news.header4',
+    defaultMessage: '[Участие в выставке «TransRussia 2015», г. Москва, МВЦ «Крокус Экспо 25.04.2015](/company/news/tr2015)'
+  },
+  'app.page.news.description4': {
+    id: 'app.page.news.description4',
+    defaultMessage: 'Участие в выставке «TransRussia 2015», г. Москва, МВЦ «Крокус Экспо'
+  },
+  'app.page.news.header5': {
+    id: 'app.page.news.header5',
+    defaultMessage: '[Сертификация ISO 9001:2009 19.05.2014](/company/news/iso)'
+  },
+  'app.page.news.description5': {
+    id: 'app.page.news.description5',
+    defaultMessage: 'Компания начала процедуру прохождения сертификации по стандарту качества ISO 9000.'
+  },
+  'app.page.news.header6': {
+    id: 'app.page.news.header6',
+    defaultMessage: '[Участие в 18-й Центрально-Азиатской выставке Transit-Kazakhstan, «Транспорт и логистика» 23.05.2014](/company/news/transit)'
+  },
+  'app.page.news.description6': {
+    id: 'app.page.news.description6',
+    defaultMessage: 'Компания приняла участие в выставке Transit-Kazakhstan.'
+  },
+  'app.page.news.header7': {
+    id: 'app.page.news.header7',
+    defaultMessage: '[Участие в 19-й международной выставки TRANS RUSSIА, 22-25 апреля 2014 года, Москва, CROCUC EXPO 24.04.2014](/company/news/crocus)'
+  },
+  'app.page.news.description7': {
+    id: 'app.page.news.description7',
+    defaultMessage: 'Участие в выставке TRANS RUSSIА'
+  },
+  'app.page.news.header8': {
+    id: 'app.page.news.header8',
+    defaultMessage: '[Получена новая награда «Звезда качества 2015» 03.08.2016](/company/news/zvezda-2015)'
+  },
+  'app.page.news.description8': {
+    id: 'app.page.news.description8',
+    defaultMessage: 'Получена новая награда «Звезда качества 2015»'
+  }
+})
