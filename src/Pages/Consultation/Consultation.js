fe0eaeddacf1e59e55ad02029092c1d38d15a5cf
Igor Borozdov <melnikow_igor@mail.ru>
2025-10-08 18:02:18 +0500

    Update Consultation page, added content

--- src/Pages/Consultation/Consultation.js
+++ src/Pages/Consultation/Consultation.js
@@ -1,18 +1,30 @@
 import React from 'react'
 import './Consultation.scss'
-// import setMessages from '../../helpers/setMessages'
-// import messages from './Consultation.messages'
+import setMessages from '../../helpers/setMessages'
+import messages from './Consultation.messages'
 import PageTitle from '../../components/PageTitle'
-import PageWithAsideMenu from '../../components/PageWithAsideMenu'
+import Page from '../../components/Page'
+import Markdown from '../../components/Markdown'
 
 const Consultation = () => {
   const classNamePrefix = 'consultation'
-  // const message = setMessages(messages, 'app.page.consultation.')
+  const message = setMessages(messages, 'app.page.consultation.')
 
   return (
-    <PageWithAsideMenu classNamePrefix={classNamePrefix} index={2}>
+    <Page classNamePrefix={classNamePrefix}>
       <PageTitle name={'consultation'} />
-    </PageWithAsideMenu>
+      <Markdown childrenClassName={{target: 'link', className: 'orange-link'}} text={message('header1')} />
+      <p className={'text-color-gray'}>{message('description1')}</p>
+
+      <Markdown childrenClassName={{target: 'link', className: 'orange-link'}} text={message('header2')} />
+      <p className={'text-color-gray'}>{message('description2')}</p>
+
+      <Markdown childrenClassName={{target: 'link', className: 'orange-link'}} text={message('header3')} />
+      <p className={'text-color-gray'}>{message('description3')}</p>
+
+      <Markdown childrenClassName={{target: 'link', className: 'orange-link'}} text={message('header4')} />
+      <p className={'text-color-gray'}>{message('description4')}</p>
+    </Page>
   )
 }
 
--- src/Pages/Consultation/Consultation.messages.js
+++ src/Pages/Consultation/Consultation.messages.js
@@ -4,5 +4,37 @@ export default defineMessages({
   'app.page.consultation.title': {
     id: 'app.page.consultation.title',
     defaultMessage: 'Консультации'
+  },
+  'app.page.consultation.header1': {
+    id: 'app.page.consultation.header1',
+    defaultMessage: '[Основные типы вагонов 21.05.2014](/consultation/wagons-types)'
+  },
+  'app.page.consultation.description1': {
+    id: 'app.page.consultation.description1',
+    defaultMessage: 'Вагонный парк состоит из двух основных групп вагонов — пассажирских и грузовых.'
+  },
+  'app.page.consultation.header2': {
+    id: 'app.page.consultation.header2',
+    defaultMessage: '[Общие сведения об устройстве вагонов 21.05.2014](/consultation/wagons)'
+  },
+  'app.page.consultation.description2': {
+    id: 'app.page.consultation.description2',
+    defaultMessage: 'Все вагоны состоят из следующих основных узлов; кузова с рамой, ходовых частей, автосцепных устройств и тормозного оборудования. Кузов служит для размещения в вагоне пассажиров или грузов.'
+  },
+  'app.page.consultation.header3': {
+    id: 'app.page.consultation.header3',
+    defaultMessage: '[Основные типы контейнеров 21.05.2014](/consultation/containers)'
+  },
+  'app.page.consultation.description3': {
+    id: 'app.page.consultation.description3',
+    defaultMessage: 'Контейнеры предназначены в основном для перевозки тарно-упаковочных и штучных грузов без тары.'
+  },
+  'app.page.consultation.header4': {
+    id: 'app.page.consultation.header4',
+    defaultMessage: '[Классификации опасных грузов на железнодорожном транспорте 21.05.2014](/consultation/dangerous-goods)'
+  },
+  'app.page.consultation.description4': {
+    id: 'app.page.consultation.description4',
+    defaultMessage: 'К опасным грузам относятся любые вещества, материалы, изделия, отходы производственной и иной деятельности, которые в силу присущих им свойств и особенностей могут при их перевозке создавать угрозу для жизни и здоровья людей, нанести вред окружающей природной среде, привести к повреждению или уничтожению материальных ценностей.'
   }
 })
