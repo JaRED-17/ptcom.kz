445fc0c59db50966d72226557b00e83560a5e377
Igor Borozdov <melnikow_igor@mail.ru>
2025-10-08 16:27:26 +0500

    Update text for WagonsTypes page

--- src/Pages/WagonsTypes/WagonsTypes.js
+++ src/Pages/WagonsTypes/WagonsTypes.js
@@ -1,14 +1,26 @@
 import React from 'react'
+import setMessages from '../../helpers/setMessages'
+import messages from './WagonsTypes.messages'
 import OurAdvantages from '../../components/OurAdvantages'
 import PageTitle from '../../components/PageTitle'
 import PageWithAsideMenu from '../../components/PageWithAsideMenu'
 
 const WagonsTypes = () => {
   const classNamePrefix = 'wagons-types'
+  const message = setMessages(messages, 'app.page.wagonsTypes.')
 
   return (
     <PageWithAsideMenu classNamePrefix={classNamePrefix} index={2}>
       <PageTitle name={'wagonsTypes'} />
+      <p className={'text-color-gray'}>{message('paragraph1')}</p>
+      <p className={'text-color-gray'}>{message('paragraph2')}</p>
+      <p className={'text-color-gray'}>{message('paragraph3')}</p>
+      <p className={'text-color-gray'}>{message('paragraph4')}</p>
+      <p className={'text-color-gray'}>{message('paragraph5')}</p>
+      <p className={'text-color-gray'}>{message('paragraph6')}</p>
+      <p className={'text-color-gray'}>{message('paragraph7')}</p>
+      <p className={'text-color-gray'}>{message('paragraph8')}</p>
+      <p className={'text-color-gray'}>{message('paragraph9')}</p>
       <OurAdvantages />
     </PageWithAsideMenu>
   )
