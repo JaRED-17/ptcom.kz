16b40fabaf6e9d24768d128b0bb907004c384546
Igor Borozdov <melnikow_igor@mail.ru>
2025-10-08 16:01:49 +0500

    Update text for DangerousGoods page

--- src/Pages/DangerousGoods/DangerousGoods.js
+++ src/Pages/DangerousGoods/DangerousGoods.js
@@ -1,14 +1,34 @@
 import React from 'react'
+import setMessages from '../../helpers/setMessages'
+import messages from './DangerousGoods.messages'
 import OurAdvantages from '../../components/OurAdvantages'
 import PageTitle from '../../components/PageTitle'
 import PageWithAsideMenu from '../../components/PageWithAsideMenu'
+import Markdown from '../../components/Markdown'
 
 const DangerousGoods = () => {
   const classNamePrefix = 'dangerous-goods'
+  const message = setMessages(messages, 'app.page.dangerousGoods.')
 
   return (
     <PageWithAsideMenu classNamePrefix={classNamePrefix} index={2}>
       <PageTitle name={'dangerousGoods'} />
+      <p className={'text-color-gray'}>{message('paragraph1')}</p>
+      <p className={'text-color-gray'}>{message('paragraph2')}</p>
+      <p className={'text-color-orange text-bolt text-header'}>{message('header1')}</p>
+      <Markdown childrenClassName={{target: 'list', className: 'text-color-gray'}} text={message('list1')} />
+      <p className={'text-color-gray'}>{message('paragraph3')}</p>
+      <Markdown childrenClassName={{target: 'paragraph', className: 'text-color-gray'}} text={message('paragraph4')} />
+      <p className={'text-color-orange text-bolt text-header'}>{message('header2')}</p>
+      <Markdown childrenClassName={{target: 'list', className: 'text-color-gray'}} text={message('list2')} />
+      <Markdown childrenClassName={{target: 'paragraph', className: 'text-color-gray'}} text={message('paragraph5')} />
+      <Markdown childrenClassName={{target: 'paragraph', className: 'text-color-gray'}} text={message('paragraph6')} />
+      <Markdown childrenClassName={{target: 'paragraph', className: 'text-color-gray'}} text={message('paragraph7')} />
+      <Markdown childrenClassName={{target: 'paragraph', className: 'text-color-gray'}} text={message('paragraph8')} />
+      <Markdown childrenClassName={{target: 'paragraph', className: 'text-color-gray'}} text={message('paragraph9')} />
+      <Markdown childrenClassName={{target: 'paragraph', className: 'text-color-gray'}} text={message('paragraph10')} />
+      <Markdown childrenClassName={{target: 'paragraph', className: 'text-color-gray'}} text={message('paragraph11')} />
+      <Markdown childrenClassName={{target: 'paragraph', className: 'text-color-gray'}} text={message('paragraph12')} />
       <OurAdvantages />
     </PageWithAsideMenu>
   )
