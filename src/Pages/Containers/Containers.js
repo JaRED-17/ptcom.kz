a53bad7c9ebdb3930ad8c3bdf39c0212ca0523fd
Igor Borozdov <melnikow_igor@mail.ru>
2025-10-08 16:12:11 +0500

    Update text for Containers page

--- src/Pages/Containers/Containers.js
+++ src/Pages/Containers/Containers.js
@@ -1,14 +1,26 @@
 import React from 'react'
+import setMessages from '../../helpers/setMessages'
+import messages from './Containers.messages'
 import OurAdvantages from '../../components/OurAdvantages'
 import PageTitle from '../../components/PageTitle'
 import PageWithAsideMenu from '../../components/PageWithAsideMenu'
 
 const Containers = () => {
   const classNamePrefix = 'containers'
+  const message = setMessages(messages, 'app.page.containers.')
 
   return (
     <PageWithAsideMenu classNamePrefix={classNamePrefix} index={2}>
       <PageTitle name={'containers'} />
+      <p className={'text-color-gray'}>{message('paragraph1')}</p>
+      <p className={'text-color-gray'}>{message('paragraph2')}</p>
+      <p className={'text-color-gray'}>{message('paragraph3')}</p>
+      <p className={'text-color-gray'}>{message('paragraph4')}</p>
+      <p className={'text-color-gray'}>{message('paragraph5')}</p>
+      <p className={'text-color-gray'}>{message('paragraph6')}</p>
+      <p className={'text-color-gray'}>{message('paragraph7')}</p>
+      <p className={'text-color-gray'}>{message('paragraph8')}</p>
+      <p className={'text-color-gray'}>{message('paragraph9')}</p>
       <OurAdvantages />
     </PageWithAsideMenu>
   )
