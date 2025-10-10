0a05594195aca01ed3fdf2bc6b02af53b3ac6321
Igor Borozdov <melnikow_igor@mail.ru>
2025-10-10 23:45:55 +0500

    Fixed reloading the site after clicking on links in the static content

--- src/components/StaticContent/StaticContent.js
+++ src/components/StaticContent/StaticContent.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react'
 import Loading from '../Loading'
 import PropTypes from 'prop-types'
+import withRoute from '../../helpers/withRoute'
 
 const StaticContent = React.memo(
-  ({ name }) => {
+  ({ name, navigate }) => {
     const classNamePrefix = 'static-content'
     const [content, setContent] = useState(null)
     const [error, setError] = useState(null)
@@ -24,14 +25,40 @@ const StaticContent = React.memo(
     return error
       ? <p>Ошибка: {error}</p>
       : content
-        ? <div className={`${classNamePrefix}__content`} dangerouslySetInnerHTML={{ __html: content }} />
+        ? (
+          <div
+            className={`${classNamePrefix}__content`}
+            onClick={(e) => {
+              const targetLink = e.target.closest('a')
+
+              if (!targetLink) return
+
+              const href = targetLink.getAttribute('href') || ''
+              const target = targetLink.getAttribute('target')
+
+              if (target && target !== '_self') return
+
+              e.preventDefault()
+
+              if (href.startsWith('http') && !href.startsWith(this.props.location.origin)) {
+                window.location.href = href
+
+                return
+              }
+
+              navigate(href)
+            }}
+            dangerouslySetInnerHTML={{ __html: content }}
+          />
+        )
         : <Loading />
   },
   (prevProps, nextProps) => prevProps.name === nextProps.name
 )
 
 StaticContent.propTypes = {
-  name: PropTypes.string
+  name: PropTypes.string,
+  navigate: PropTypes.object
 }
 
-export default StaticContent
+export default withRoute(StaticContent)
