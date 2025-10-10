7d4398b67b7794bd4361b7e20f63ed2a13fd4168
Igor Borozdov <melnikow_igor@mail.ru>
2025-10-10 23:04:56 +0500

    fixed the issue when content wasn't loaded after leaving from static page and open another static page

--- src/Pages/StaticPage/StaticPage.js
+++ src/Pages/StaticPage/StaticPage.js
@@ -5,37 +5,40 @@ import PageWithAsideMenu from '../../components/PageWithAsideMenu'
 import Loading from '../../components/Loading'
 import PropTypes from 'prop-types'
 
-const StaticPage = ({ name }) => {
-  const classNamePrefix = 'static-page'
-  const [content, setContent] = useState(null)
-  const [error, setError] = useState(null)
+const StaticPage = React.memo(
+  ({ name }) => {
+    const classNamePrefix = 'static-page'
+    const [content, setContent] = useState(null)
+    const [error, setError] = useState(null)
 
-  useEffect(() => {
-    fetch('/cms/static/' + name + '/ru.html')
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok')
+    useEffect(() => {
+      fetch('/cms/static/' + name + '/ru.html')
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error('Network response was not ok')
+          }
+
+          return response.text()
+        })
+        .then(html => setContent(html))
+        .catch(err => setError(err.message))
+    }, [name])
+
+    return (
+      <PageWithAsideMenu classNamePrefix={classNamePrefix} index={0}>
+        <PageTitle name={name} />
+        {error
+          ? <p>Ошибка: {error}</p>
+          : content
+            ? <div className={`${classNamePrefix}__content`} dangerouslySetInnerHTML={{ __html: content }} />
+            : <Loading />
         }
-
-        return response.text()
-      })
-      .then(html => setContent(html))
-      .catch(err => setError(err.message))
-  })
-
-  return (
-    <PageWithAsideMenu classNamePrefix={classNamePrefix} index={0}>
-      <PageTitle name={name} />
-      {error
-        ? <p>Ошибка: {error}</p>
-        : content
-          ? <div className={`${classNamePrefix}__content`} dangerouslySetInnerHTML={{ __html: content }} />
-          : <Loading />
-      }
-      <OurAdvantages />
-    </PageWithAsideMenu>
-  )
-}
+        <OurAdvantages />
+      </PageWithAsideMenu>
+    )
+  },
+  (prevProps, nextProps) => prevProps.name === nextProps.name
+)
 
 StaticPage.propTypes = {
   name: PropTypes.string
