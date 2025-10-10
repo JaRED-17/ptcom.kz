60ddfdc506d68eb138d3c91c6e59df3390290738
Igor Borozdov <melnikow_igor@mail.ru>
2025-10-10 22:54:31 +0500

    Added fetching StaffList from html

--- src/components/StaffList/StaffList.js
+++ src/components/StaffList/StaffList.js
@@ -1,45 +1,30 @@
 import React, {useEffect, useState } from 'react'
 import './StaffList.scss'
-import messages from './StaffList.messages'
-import setMessages from '../../helpers/setMessages'
-import ContactBlock from './ContactBlock'
 import Loading from '../Loading'
 
 const StaffList = () => {
   const classNamePrefix = 'staff-list'
-  const message = setMessages(messages, 'app.staff.list.')
   const [employees, setEmployees] = useState(null)
   const [error, setError] = useState(null)
 
   useEffect(() => {
-    fetch('/cms/data/employees.json')
+    fetch('/cms/static/staffs/ru.html')
       .then((response) => {
         if (!response.ok) {
           throw new Error('Network response was not ok')
         }
-        return response.json()
+
+        return response.text()
       })
-      .then(json => setEmployees(json))
+      .then(html => setEmployees(html))
       .catch(err => setError(err.message))
   }, [])
 
-  return (
-    <div className={classNamePrefix}>
-      {error
-        ? <p>Ошибка: {error}</p>
-        : employees
-          ? employees.map((employee, index) => (
-            <ContactBlock
-              key={index}
-              name={message(employee.name)}
-              position={message(employee.position)}
-              contact={message(employee.contact)}
-            />
-          ))
-          : <Loading />
-      }
-    </div>
-  )
+  return error
+    ? <p>Ошибка: {error}</p>
+    : employees
+      ? <div className={`${classNamePrefix}`} dangerouslySetInnerHTML={{ __html: employees }} />
+      : <Loading />
 }
 
 export default StaffList
