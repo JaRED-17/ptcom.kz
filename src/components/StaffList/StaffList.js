cfa701645be982e3e625d8b05980bf26cd763328
Igor Borozdov <melnikow_igor@mail.ru>
2025-10-10 23:18:29 +0500

    Update StaffList to use StaticContent component

--- src/components/StaffList/StaffList.js
+++ src/components/StaffList/StaffList.js
@@ -1,30 +1,15 @@
-import React, {useEffect, useState } from 'react'
+import React from 'react'
 import './StaffList.scss'
-import Loading from '../Loading'
+import StaticContent from '../StaticContent'
 
 const StaffList = () => {
   const classNamePrefix = 'staff-list'
-  const [employees, setEmployees] = useState(null)
-  const [error, setError] = useState(null)
 
-  useEffect(() => {
-    fetch('/cms/static/staffs/ru.html')
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok')
-        }
-
-        return response.text()
-      })
-      .then(html => setEmployees(html))
-      .catch(err => setError(err.message))
-  }, [])
-
-  return error
-    ? <p>Ошибка: {error}</p>
-    : employees
-      ? <div className={`${classNamePrefix}`} dangerouslySetInnerHTML={{ __html: employees }} />
-      : <Loading />
+  return (
+    <div className={classNamePrefix}>
+      <StaticContent name={'staffs'} />
+    </div>
+  )
 }
 
 export default StaffList
