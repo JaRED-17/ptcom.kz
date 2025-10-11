6156575f58c0b0e6aafa0ef12d90f7205c6d06a7
Igor Borozdov <melnikow_igor@mail.ru>
2025-10-11 18:15:24 +0500

    Revert "Update the folder for the production build"
    This reverts commit 7481f30d5a91f35868f62710c3191da8e7b1a65b.

--- server.js
+++ server.js
@@ -15,10 +15,10 @@ const PORT = process.env.PORT || 3000
 app.use(webpackDevMiddleware(compiler, { publicPath: config.output.publicPath }))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
-app.use(express.static('build'))
+app.use(express.static('dist'))
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
+  res.sendFile(path.resolve(__dirname, 'dist', 'index.html'))
 })
 
 app.post('/api/send-email', async (req, res) => {
