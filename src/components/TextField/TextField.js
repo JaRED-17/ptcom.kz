3970b69284a5c495721c3c2606c812dec4751335
Igor Borozdov <melnikow_igor@mail.ru>
2025-10-11 20:29:07 +0500

    Added TextField component

---
+++ src/components/TextField/TextField.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import './TextField.scss'
+import {TextField as TextFieldUI} from '@mui/material'
+import PropTypes from 'prop-types'
+
+const TextField = ({ label, name, value, onChange = () => {}, required, fullWidth, rows, children }) => {
+  return (
+    <TextFieldUI
+      className={`input input-default`}
+      label={label}
+      name={name}
+      value={value}
+      onChange={onChange}
+      required={required}
+      fullWidth={fullWidth}
+      rows={rows}
+    >
+      {children}
+    </TextFieldUI>
+  )
+}
+
+TextField.propTypes = {
+  label: PropTypes.string,
+  name: PropTypes.string,
+  value: PropTypes.object,
+  onChange: PropTypes.func,
+  required: PropTypes.bool,
+  fullWidth: PropTypes.bool,
+  rows: PropTypes.number,
+  children: PropTypes.any
+}
+
+export default TextField
