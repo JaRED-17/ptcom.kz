8134ccd37b964d1f7fe5b1a5aa66dd2efc4b13e9
Igor Borozdov <melnikow_igor@mail.ru>
2025-10-11 15:03:02 +0500

    Fixed an issue when animation didn't fire for static content

--- src/components/PageWithAsideMenu/PageWithAsideMenu.js
+++ src/components/PageWithAsideMenu/PageWithAsideMenu.js
@@ -1,16 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
 import AsideMenu from '../AsideMenu'
 import Page from '../Page'
 import pages from '../../cms/data/pages.json'
 
+const AnimatedWrapper = ({ children }) => {
+  const [child, setChild] = useState(null)
+  const [key, setKey] = useState(0)
+
+  useEffect(() => {
+    setChild(children)
+    setKey(prev => prev + 1)
+  }, [children])
+
+  return child ? (
+    <div key={key} className={'fade-in-up'}>
+      {child}
+    </div>
+  ) : <div />
+}
+
+AnimatedWrapper.propTypes = {
+  children: PropTypes.any
+}
+
 const PageWithAsideMenu = ({ classNamePrefix, children, index = 0, entries }) => {
   return (
     <Page classNamePrefix={classNamePrefix} asideMenu>
       <AsideMenu list={entries ? pages[index].entries[entries].entries : pages[index].entries} />
-      <div className={'fade-in-up'}>
-        {children}
-      </div>
+      <AnimatedWrapper children={children} />
     </Page>
   )
 }
