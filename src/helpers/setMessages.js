6eef0ae7d71a2f6d94dacf1a73561889ddbb70d4
Igor Borozdov <melnikow_igor@mail.ru>
2025-10-12 16:16:31 +0500

    Added replacements

--- src/helpers/setMessages.js
+++ src/helpers/setMessages.js
@@ -3,9 +3,9 @@ import { useIntl } from 'react-intl'
 export default (messages, prefix) => {
   const intl = useIntl()
 
-  return (postfix) => {
+  return (postfix, replacements) => {
     const message = messages[prefix + postfix]
 
-    return message ? intl.formatMessage(message) : prefix + postfix
+    return message ? intl.formatMessage(message, replacements) : prefix + postfix
   }
 }
