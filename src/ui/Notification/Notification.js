05aa843a02fb9cf87070f9a519c61f0c9d748f3c
Igor Borozdov <melnikow_igor@mail.ru>
2025-10-13 18:56:12 +0500

    Fixed an error with state before closing notification

--- src/ui/Notification/Notification.js
+++ src/ui/Notification/Notification.js
@@ -1,27 +1,35 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import PropTypes from 'prop-types'
 import IconButton from '@mui/material/IconButton'
 import CloseIcon from '@mui/icons-material/Close'
-import {useSnackbar} from 'notistack'
+import { useSnackbar } from 'notistack'
 
 const Notification = ({ open, variant, autoHideDuration = 3000, onClose, children }) => {
   const { enqueueSnackbar, closeSnackbar } = useSnackbar()
 
-  return open ? (
-    enqueueSnackbar(children, {
-      variant: variant,
-      autoHideDuration: autoHideDuration,
-      preventDuplicate: true,
-      onClose: (event, reason, key) => {
-        onClose(event, reason, key)
-      },
-      action: (key) => (
-        <IconButton onClick={() => closeSnackbar(key)} color={'inherit'}>
-          <CloseIcon fontSize={'small'} />
-        </IconButton>
-      )
-    })
-  ) : null
+  useEffect(() => {
+    if (open) {
+      const key = enqueueSnackbar(children, {
+        variant,
+        autoHideDuration,
+        preventDuplicate: true,
+        onClose: (event, reason, key) => {
+          onClose?.(event, reason, key)
+        },
+        action: (key) => (
+          <IconButton onClick={() => closeSnackbar(key)} color={'inherit'}>
+            <CloseIcon fontSize={'small'} />
+          </IconButton>
+        )
+      })
+
+      return () => {
+        closeSnackbar(key)
+      }
+    }
+  }, [open])
+
+  return null
 }
 
 Notification.propTypes = {
