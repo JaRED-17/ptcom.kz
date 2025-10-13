c81408c8632d0ffad3d29bf16c61de6c9dae5c75
Igor Borozdov <melnikow_igor@mail.ru>
2025-10-13 22:41:29 +0500

    Update folder name for the production

--- server.js
+++ server.js
@@ -32,9 +32,9 @@ if (env === 'development') {
     })
   })
 } else {
-  app.use(express.static('dist'))
+  app.use(express.static('build'))
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'dist', 'index.html'))
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
   })
 }
 
