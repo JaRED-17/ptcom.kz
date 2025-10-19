721688e36e89aa06c751e40cb1c2edbe035ad5d3
Igor Borozdov <igor.borozdov@finnplay.com>
2025-10-19 14:54:07 +0500

    Update keywords for each page

--- src/components/CustomHelmet/CustomHelmet.messages.js
+++ src/components/CustomHelmet/CustomHelmet.messages.js
@@ -16,5 +16,113 @@ export default defineMessages({
   'app.page.seo.title.template': {
     id: 'app.page.seo.title.template',
     defaultMessage: '{title} | Петропавл Экспресс Логистика'
+  },
+  'app.page.seo.keywords.aboutUs': {
+    id: 'app.page.seo.keywords.aboutUs',
+    defaultMessage: 'о компании'
+  },
+  'app.page.seo.keywords.staffs': {
+    id: 'app.page.seo.keywords.staffs',
+    defaultMessage: 'сотрудники'
+  },
+  'app.page.seo.keywords.news': {
+    id: 'app.page.seo.keywords.news',
+    defaultMessage: 'новости'
+  },
+  'app.page.seo.keywords.licenses': {
+    id: 'app.page.seo.keywords.licenses',
+    defaultMessage: 'лицензии'
+  },
+  'app.page.seo.keywords.vacancies': {
+    id: 'app.page.seo.keywords.vacancies',
+    defaultMessage: 'вакансии'
+  },
+  'app.page.seo.keywords.stRkIso90012009': {
+    id: 'app.page.seo.keywords.stRkIso90012009',
+    defaultMessage: 'СТ РК ИСО 9001–2009'
+  },
+  'app.page.seo.keywords.5years': {
+    id: 'app.page.seo.keywords.5years',
+    defaultMessage: 'компания, 5 лет на рынке!'
+  },
+  'app.page.seo.keywords.tl2015': {
+    id: 'app.page.seo.keywords.tl2015',
+    defaultMessage: 'Участие в выставке Транспорт и Логистика, г. Алматы.'
+  },
+  'app.page.seo.keywords.tr2015': {
+    id: 'app.page.seo.keywords.tr2015',
+    defaultMessage: 'Участие в выставке TransRussia 2015, г. Москва МВЦ Крокус Экспо'
+  },
+  'app.page.seo.keywords.iso': {
+    id: 'app.page.seo.keywords.iso',
+    defaultMessage: 'Сертификация ISO 9001:2009'
+  },
+  'app.page.seo.keywords.transit': {
+    id: 'app.page.seo.keywords.transit',
+    defaultMessage: 'Участие в 18-й Центрально-Азиатской выставке Transit-Kazakhstan, Транспорт и логистика'
+  },
+  'app.page.seo.keywords.crocus': {
+    id: 'app.page.seo.keywords.crocus',
+    defaultMessage: 'Участие в выставке TRANS RUSSIА'
+  },
+  'app.page.seo.keywords.zvezda2015': {
+    id: 'app.page.seo.keywords.zvezda2015',
+    defaultMessage: 'Получена новая награда, Звезда качества 2015, Получена новая награда Звезда качества 2015'
+  },
+  'app.page.seo.keywords.services': {
+    id: 'app.page.seo.keywords.services',
+    defaultMessage: 'услуги'
+  },
+  'app.page.seo.keywords.provisionOfWagons': {
+    id: 'app.page.seo.keywords.provisionOfWagons',
+    defaultMessage: 'предоставление вагонов'
+  },
+  'app.page.seo.keywords.paymentRailwayTariffs': {
+    id: 'app.page.seo.keywords.paymentRailwayTariffs',
+    defaultMessage: 'оплата ЖД тарифов'
+  },
+  'app.page.seo.keywords.driveWays': {
+    id: 'app.page.seo.keywords.driveWays',
+    defaultMessage: 'подъездные пути'
+  },
+  'app.page.seo.keywords.schema': {
+    id: 'app.page.seo.keywords.schema',
+    defaultMessage: 'разработка схем погрузки, согласование схем погрузки, разработка и согласование схем погрузки'
+  },
+  'app.page.seo.keywords.handlingDostyk': {
+    id: 'app.page.seo.keywords.handlingDostyk',
+    defaultMessage: 'перегруз груза на ст. Достык, станция Достык'
+  },
+  'app.page.seo.keywords.cargoProcessing': {
+    id: 'app.page.seo.keywords.cargoProcessing',
+    defaultMessage: 'грузопереработка'
+  },
+  'app.page.seo.keywords.consultation': {
+    id: 'app.page.seo.keywords.consultation',
+    defaultMessage: 'консультации'
+  },
+  'app.page.seo.keywords.wagonsTypes': {
+    id: 'app.page.seo.keywords.wagonsTypes',
+    defaultMessage: 'основные типы вагонов, типы вагонов'
+  },
+  'app.page.seo.keywords.wagons': {
+    id: 'app.page.seo.keywords.wagons',
+    defaultMessage: 'общие сведения об устройстве вагонов, устройство вагонов'
+  },
+  'app.page.seo.keywords.containers': {
+    id: 'app.page.seo.keywords.containers',
+    defaultMessage: 'основные типы контейнеров, типы контейнеров'
+  },
+  'app.page.seo.keywords.dangerousGoods': {
+    id: 'app.page.seo.keywords.dangerousGoods',
+    defaultMessage: 'опасные грузы'
+  },
+  'app.page.seo.keywords.calculation': {
+    id: 'app.page.seo.keywords.calculation',
+    defaultMessage: 'расчет тарифов'
+  },
+  'app.page.seo.keywords.contacts': {
+    id: 'app.page.seo.keywords.contacts',
+    defaultMessage: 'контакты'
   }
 })
