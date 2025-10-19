f33606dd75b97dae7847ee2249f788cc9871be97
Igor Borozdov <igor.borozdov@finnplay.com>
2025-10-19 16:42:58 +0500

    Fix eslint error

--- src/helpers/withRoute.js
+++ src/helpers/withRoute.js
@@ -2,11 +2,16 @@ import React from 'react'
 import { useNavigate } from 'react-router-dom'
 
 const withRoute = (Wrapper) => {
-  return (props) => {
+  const ComponentWithRoute = (props) => {
     const navigate = useNavigate()
 
     return <Wrapper {...props} navigate={navigate} />
   }
+
+  const wrapperName = Wrapper.displayName || Wrapper.name || 'Component'
+  ComponentWithRoute.displayName = `withRoute(${wrapperName})`
+
+  return ComponentWithRoute
 }
 
 export default withRoute
