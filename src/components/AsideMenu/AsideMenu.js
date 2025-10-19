b77918a108d9a1baca44fde181d6ce0d98e348d4
Igor Borozdov <igor.borozdov@finnplay.com>
2025-10-19 20:31:14 +0500

    Fixed active class for the aside menu if there is language in the url

--- src/components/AsideMenu/AsideMenu.js
+++ src/components/AsideMenu/AsideMenu.js
@@ -4,16 +4,26 @@ import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
 import setMessages from '../../helpers/setMessages'
 import messages from '../PageTitle/PageTitle.messages'
+import language from '../../helpers/language'
 
 const AsideMenu = ({ list }) => {
   const classNamePrefix = 'aside-menu'
   const message = setMessages(messages, 'app.page.')
+  const activeClass = (url) => {
+    const currentLanguage = language.getCurrentLanguage()
+    const currentPath = window.location.pathname
+    const languagePrefixRegexp = new RegExp(`^/${currentLanguage}($|/)`)
+
+    return languagePrefixRegexp.test(currentPath)
+      ? currentPath === `/${currentLanguage}${url}`
+      : currentPath === url
+  }
 
   return (
     <div className={classNamePrefix}>
       {(() => {
         return list.map((menuItem, index) => {
-          return <Link key={index} to={menuItem.url} className={`white-link${window.location.pathname === menuItem.url ? ' active' : ''}`}>
+          return <Link key={index} to={menuItem.url} className={`white-link${activeClass(menuItem.url) ? ' active' : ''}`}>
             {message(menuItem.name + '.title')}
           </Link>
         })
