731721c39a30f9eae61c5f011cdfee2b0379b96c
Igor Borozdov <igor.borozdov@finnplay.com>
2025-10-27 17:00:47 +0500

    Added AutocompleteField component

---
+++ src/ui/AutocompleteField/AutocompleteField.js
@@ -0,0 +1,110 @@
+import React, { useEffect, useRef, useState } from 'react'
+import Autocomplete from '@mui/material/Autocomplete'
+import PropTypes from 'prop-types'
+import { TextField } from '@mui/material'
+import ErrorMessage from '../../components/ErrorMessage'
+import validate from '../../helpers/validate'
+
+const AutocompleteField = ({
+  options = [],
+  label,
+  name,
+  type,
+  fullWidth = true,
+  multiline = false,
+  rows,
+  disabled = false,
+  mandatory = false,
+  readOnly = false,
+  addRef = () => {}
+}) => {
+  const inputRef = useRef(null)
+  const [error, setError] = useState({
+    errorCode: '',
+    replacements: {}
+  })
+  const [value, setValue] = useState('')
+  const [inputValue, setInputValue] = useState('')
+
+  useEffect(() => {
+    inputRef.current.validateField = validateField
+    inputRef.current.clearError = () => {
+      setError({
+        errorCode: '',
+        replacements: {}
+      })
+    }
+    inputRef.current.clear = () => {
+      setValue('')
+      setInputValue('')
+    }
+    addRef(name, inputRef)
+  }, [])
+
+  const validateField = () => {
+    const result = validate[name]?.(inputRef.current.value)
+
+    if (result.error) {
+      setError({ ...result.errorDetails })
+    } else {
+      setError({
+        errorCode: '',
+        replacements: {}
+      })
+    }
+
+    return result
+  }
+
+  return (
+    <Autocomplete
+      options={options}
+      getOptionLabel={(option) => option}
+      value={value}
+      inputValue={inputValue}
+      onChange={(event, newValue) => {
+        setValue(newValue)
+      }}
+      onInputChange={(event, newValue) => {
+        setInputValue(newValue)
+      }}
+      renderInput={(params) => (
+        <TextField
+          {...params}
+          inputRef={inputRef}
+          className={'input input-default'}
+          label={mandatory ? `${label}*` : label}
+          name={name}
+          type={type}
+          onBlur={validateField}
+          disabled={disabled}
+          fullWidth={fullWidth}
+          multiline={multiline}
+          error={!!error.errorCode}
+          helperText={error.errorCode ? <ErrorMessage code={error.errorCode} replacements={error.replacements} /> : ''}
+          rows={rows}
+          inputProps={{
+            ...params.inputProps,
+            readOnly
+          }}
+        />
+      )}
+    />
+  )
+}
+
+AutocompleteField.propTypes = {
+  options: PropTypes.object,
+  label: PropTypes.string,
+  name: PropTypes.string,
+  type: PropTypes.string,
+  fullWidth: PropTypes.bool,
+  multiline: PropTypes.bool,
+  rows: PropTypes.number,
+  disabled: PropTypes.bool,
+  mandatory: PropTypes.bool,
+  readOnly: PropTypes.bool,
+  addRef: PropTypes.func
+}
+
+export default AutocompleteField
