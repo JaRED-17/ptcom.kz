48acccf453835ed076b254a7f7d9ff7626758d49
Igor Borozdov <igor.borozdov@finnplay.com>
2025-10-27 17:06:52 +0500

    Update TextField component

--- src/ui/TextField/TextField.js
+++ src/ui/TextField/TextField.js
@@ -1,60 +1,48 @@
-import React, { forwardRef, useEffect, useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import './TextField.scss'
-import { Checkbox, FormControlLabel, FormGroup, FormHelperText, TextField as TextFieldUI } from '@mui/material'
+import { TextField as TextFieldUI } from '@mui/material'
 import ErrorMessage from '../../components/ErrorMessage'
-import PropTypes from 'prop-types'
 import validate from '../../helpers/validate'
-import { IMaskInput } from 'react-imask'
-import Autocomplete from '@mui/material/Autocomplete'
-import UTSNG from '../../cms/data/catalogs/UTSNG.json'
-import typeWagons from '../../cms/data/catalogs/typeWagons.json'
-import Markdown from '../../components/Markdown'
-
-const PhoneMask = forwardRef(function PhoneMask (props, ref) {
-  return (
-    <IMaskInput
-      {...props}
-      mask={'+{7} (000) 000-00-00'}
-      lazy={false}
-      placeholderChar={'_'}
-      definitions={{
-        '#': /[0-9]/
-      }}
-      inputRef={ref}
-      overwrite
-    />
-  )
-})
+import PropTypes from 'prop-types'
 
 const TextField = ({
   label,
   name,
   type,
-  value,
-  onChange = () => {},
   fullWidth = true,
   multiline = false,
   rows,
   disabled = false,
   mandatory = false,
-  addRef = () => {}
+  addRef = () => {},
+  params
 }) => {
   const inputRef = useRef(null)
   const [error, setError] = useState({
     errorCode: '',
     replacements: {}
   })
+  const [value, setValue] = useState('')
 
   useEffect(() => {
     inputRef.current.validateField = validateField
+    inputRef.current.clearError = () => {
+      setError({
+        errorCode: '',
+        replacements: {}
+      })
+    }
+    inputRef.current.clear = () => {
+      setValue('')
+    }
     addRef(name, inputRef)
   }, [])
 
   const validateField = () => {
-    const result = name === 'policy' ? validate[name](inputRef.current.checked) : validate[name](inputRef.current.value)
+    const validation = validate[name](inputRef.current.value)
 
-    if (result.error) {
-      setError({ ...result.errorDetails })
+    if (validation.error) {
+      setError({ ...validation.errorDetails })
     } else {
       setError({
         errorCode: '',
@@ -62,109 +50,41 @@ const TextField = ({
       })
     }
 
-    return result
+    return validation
   }
 
-  const textField = (params) => {
-    return (
-      <TextFieldUI
-        {...params}
-        inputRef={inputRef}
-        className={'input input-default'}
-        label={mandatory ? `${label}*` : label}
-        name={name}
-        type={type}
-        value={value}
-        onChange={onChange}
-        onBlur={validateField}
-        disabled={disabled}
-        fullWidth={fullWidth}
-        multiline={multiline}
-        error={!!error.errorCode}
-        helperText={error.errorCode ? <ErrorMessage code={error.errorCode} replacements={error.replacements} /> : ''}
-        rows={rows}
-      />
-    )
-  }
-
-  if (name === 'cargoCodes') {
-    return (
-      <Autocomplete
-        options={UTSNG}
-        getOptionLabel={(option) => `${option.code} ${option.name}`}
-        value={value}
-        renderInput={(params) => textField(params)}
-      />
-    )
-  }
-
-  if (name === 'weightName') {
-    const result = []
-
-    UTSNG.forEach((item) => {
-      if (!result.includes(item.name)) {
-        result.push(item.name)
-      }
-    })
-
-    return (
-      <Autocomplete
-        options={result.sort((a, b) => a - b)}
-        getOptionLabel={(option) => option}
-        value={value}
-        renderInput={(params) => textField(params)}
-      />
-    )
-  }
-
-  if (type === 'checkbox') {
-    return (
-      <FormGroup className={'input input-default'}>
-        <FormControlLabel
-          control={<Checkbox inputRef={inputRef} checked={value} onChange={validateField} />}
-          label={<Markdown childrenClassName={{ target: 'link', className: 'orange-link' }} text={mandatory ? `${label}*` : label} params={{ target: '_blank' }} />}
-        />
-        {error.errorCode ? (
-          <FormHelperText error>
-            <ErrorMessage code={error.errorCode} replacements={error.replacements} />
-          </FormHelperText>
-        ) : null}
-      </FormGroup>
-    )
-  }
-
-  if (name === 'typeWagons') {
-    return (
-      <Autocomplete
-        options={typeWagons}
-        getOptionLabel={option => option.type}
-        value={value}
-        renderInput={(params) => textField({
-          ...params,
-          inputProps: {
-            ...params.inputProps,
-            readOnly: true
-          }
-        })}
-      />
-    )
-  }
-
-  return textField({ InputProps: name === 'phone' ? { inputComponent: PhoneMask } : null })
+  return (
+    <TextFieldUI
+      {...params}
+      inputRef={inputRef}
+      className={'input input-default'}
+      label={mandatory ? `${label}*` : label}
+      name={name}
+      type={type}
+      value={value}
+      onChange={(e) => setValue(e.target.value)}
+      onBlur={validateField}
+      disabled={disabled}
+      fullWidth={fullWidth}
+      multiline={multiline}
+      error={!!error.errorCode}
+      helperText={error.errorCode ? <ErrorMessage code={error.errorCode} replacements={error.replacements} /> : ''}
+      rows={rows}
+    />
+  )
 }
 
 TextField.propTypes = {
   label: PropTypes.string,
   name: PropTypes.string,
   type: PropTypes.string,
-  value: PropTypes.object,
-  onChange: PropTypes.func,
   fullWidth: PropTypes.bool,
   multiline: PropTypes.bool,
   rows: PropTypes.number,
   disabled: PropTypes.bool,
   mandatory: PropTypes.bool,
-  addRef: PropTypes.func
+  addRef: PropTypes.func,
+  params: PropTypes.object
 }
 
 export default TextField
