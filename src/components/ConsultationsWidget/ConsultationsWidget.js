cdf10437ba17a029fdaf4d17b25a9c07231b6b95
Igor Borozdov <melnikow_igor@mail.ru>
2025-10-28 17:20:42 +0500

    Added ConsultationsWidget component

---
+++ src/components/ConsultationsWidget/ConsultationsWidget.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import './ConsultationsWidget.scss'
+import pages from '../../cms/data/pages.json'
+import { Link } from 'react-router-dom'
+import messages from './ConsultationsWidget.messages'
+import PageTitles from '../PageTitle/PageTitle.messages'
+import setMessages from '../../helpers/setMessages'
+
+const ConsultationsWidget = () => {
+  const classNamePrefix = 'consultations-widget'
+  const message = setMessages(messages, 'app.consultationsWidget.')
+  const PageTitle = setMessages(PageTitles, 'app.page.')
+
+  return (
+    <div className={classNamePrefix}>
+      <div className={`${classNamePrefix}__header`}>
+        <h1>{message('title')}</h1>
+      </div>
+      <div className={`${classNamePrefix}__links`}>
+        {pages[2].entries.map((entry, index) => (
+          <div className={`${classNamePrefix}__link`} key={index}>
+            <Link className={'grey-link'} to={entry.url}>{PageTitle(entry.name + '.title')}</Link>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default ConsultationsWidget
---
+++ src/components/ConsultationsWidget/ConsultationsWidget.messages.js
@@ -0,0 +1,8 @@
+import { defineMessages } from 'react-intl'
+
+export default defineMessages({
+  'app.consultationsWidget.title': {
+    id: 'app.consultationsWidget.title',
+    defaultMessage: 'Консультации'
+  }
+})
