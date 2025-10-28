97c3a1efacb7cf59af79e0c8badb3522501f5305
Igor Borozdov <melnikow_igor@mail.ru>
2025-10-28 15:43:25 +0500

    Introduce classNamePrefix for Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,31 +6,32 @@ import Markdown from '../Markdown'
 import LanguageSelector from '../../ui/LanguageSelector'
 
 const Footer = () => {
+  const classNamePrefix = 'footer'
   const message = setMessages(messages, 'app.footer.')
 
   return (
-    <div className={'footer'}>
-      <div className={'footer__content'}>
-        <div className={'footer__block footer_about-us'}>
-          <h4 className={'footer__block-header'}>
+    <div className={classNamePrefix}>
+      <div className={`${classNamePrefix}__content`}>
+        <div className={`${classNamePrefix}__block footer_about-us`}>
+          <h4 className={`${classNamePrefix}__block-header`}>
             {message('about-us.title')}
           </h4>
           <p>{message('about-us.text')}</p>
         </div>
-        <div className={'footer__block footer_contacts'}>
-          <h4 className={'footer__block-header'}>
+        <div className={`${classNamePrefix}__block footer_contacts`}>
+          <h4 className={`${classNamePrefix}__block-header`}>
             {message('contacts.title')}
           </h4>
           <Markdown text={message('contacts.text')} />
         </div>
-        <div className={'footer__block footer_address'}>
-          <h4 className={'footer__block-header'}>
+        <div className={`${classNamePrefix}__block footer_address`}>
+          <h4 className={`${classNamePrefix}__block-header`}>
             {message('address.title')}
           </h4>
           <p>{message('address.text')}</p>
         </div>
-        <div className={'footer__block footer_navigation'}>
-          <h4 className={'footer__block-header'}>
+        <div className={`${classNamePrefix}__block footer_navigation`}>
+          <h4 className={`${classNamePrefix}__block-header`}>
             {message('navigation.title')}
           </h4>
           <Markdown
