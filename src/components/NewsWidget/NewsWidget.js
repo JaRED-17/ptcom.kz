5f08c7a24a4a85446094c39d16da6fc27bb65249
Igor Borozdov <melnikow_igor@mail.ru>
2025-10-28 17:12:53 +0500

    Added NewsWidget component

---
+++ src/components/NewsWidget/NewsWidget.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import './NewsWidget.scss'
+import pages from '../../cms/data/pages.json'
+import { Link } from 'react-router-dom'
+import messages from './NewsWidget.messages'
+import PageTitles from '../PageTitle/PageTitle.messages'
+import setMessages from '../../helpers/setMessages'
+
+const NewsWidget = () => {
+  const classNamePrefix = 'news-widget'
+  const message = setMessages(messages, 'app.newsWidget.')
+  const PageTitle = setMessages(PageTitles, 'app.page.')
+
+  return (
+    <div className={classNamePrefix}>
+      <div className={`${classNamePrefix}__header`}>
+        <h1>{message('title')}</h1>
+      </div>
+      <div className={`${classNamePrefix}__links`}>
+        {pages[0].entries[1].entries.map((entry, index) => (
+          <div className={`${classNamePrefix}__link`} key={index}>
+            <Link className={'white-link'} to={entry.url}>{PageTitle(entry.name + '.title')}</Link>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default NewsWidget
---
+++ src/components/NewsWidget/NewsWidget.messages.js
@@ -0,0 +1,8 @@
+import { defineMessages } from 'react-intl'
+
+export default defineMessages({
+  'app.newsWidget.title': {
+    id: 'app.newsWidget.title',
+    defaultMessage: 'Новости'
+  }
+})
